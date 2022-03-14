cd9a4594aebb90e76b1e5ea5585ed1eb8abcb7c1
G2357 <noel.j7178@gmail.com>
2022-03-14 00:07:08 -0600

    user navigation fix on create/update

--- section_3_exercises/rails_6_projects/alpha_blog/app/controllers/users_controller.rb
+++ section_3_exercises/rails_6_projects/alpha_blog/app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome to AlphaBlog #{@user.username}, you have succesfully signed up!"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render "new"
     end
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   def update
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render "edit"
     end
