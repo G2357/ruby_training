e720febcdaa25a0028718349db6b816439864036
G2357 <noel.j7178@gmail.com>
2022-03-13 21:55:47 -0600

    add user Sign up action

--- section_3_exercises/rails_6_projects/alpha_blog/config/routes.rb
+++ section_3_exercises/rails_6_projects/alpha_blog/config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get "about", to: "pages#about"
   # resources :articles, only: [:index, :show, :new, :create, :edit, :update, :destroy] # All the available routes on resouces
   resources :articles
+  get "signup", to: "users#new"
+  resources :users, exceot: [:new]
 end
