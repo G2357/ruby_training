2642987653e8edc47f35e5954f17ec3ebe4ed72b
G2357 <noel.j7178@gmail.com>
2022-03-16 23:48:19 -0600

    session storage implemented

--- section_3_exercises/rails_6_projects/alpha_blog/app/controllers/users_controller.rb
+++ section_3_exercises/rails_6_projects/alpha_blog/app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to AlphaBlog #{@user.username}, you have succesfully signed up!"
       redirect_to user_path(@user)
     else
